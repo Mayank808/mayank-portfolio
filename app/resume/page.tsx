eb1d51c0806bbdba23ed89fd6185cfef5b1b9a4b
Mayank808 <mayankmehra628@gmail.com>
2024-06-20 23:28:50 -0400

    updated resume view to embedding to test on mobile

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,8 +1,10 @@
 export default function Resume() {
     return (
-        <object data="/resume.pdf" type="application/pdf" width="100%" height="100%" className="absolute w-full h-full">
-            <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1c04kDYCC_Ggf7gSFy-_SuzEDglEmaFnk/view?usp=sharing">to the PDF!</a></p>
-        </object>
+        // <iframe className="absolute w-full h-full" src='/resume.pdf'/>
+        <embed src='/resume.pdf' type="application/pdf" width="100%" height="100%" className="absolute w-full h-full" />
+        // <object data="/resume.pdf" type="application/pdf" width="100%" height="100%" className="absolute w-full h-full">
+        //     <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1c04kDYCC_Ggf7gSFy-_SuzEDglEmaFnk/view?usp=sharing">to the PDF!</a></p>
+        // </object>
     );
   }
   
