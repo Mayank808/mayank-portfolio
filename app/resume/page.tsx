2094a60b482251faa28da182b3124beadd69cbcd
Mayank808 <mayankmehra628@gmail.com>
2024-06-20 23:31:12 -0400

    reverting back to iframe for mobile

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,9 @@
 export default function Resume() {
     return (
-        // <iframe className="absolute w-full h-full" src='/resume.pdf'/>
-        <embed src='/resume.pdf' type="application/pdf" width="100%" height="100%" className="absolute w-full h-full" />
+        <iframe className="absolute w-full h-full" src='/resume.pdf'/>
+
+        // Below methods do not work on mobile (safari)
+        // <embed src='/resume.pdf' type="application/pdf" width="100%" height="100%" className="absolute w-full h-full" />
         // <object data="/resume.pdf" type="application/pdf" width="100%" height="100%" className="absolute w-full h-full">
         //     <p>Alternative text - include a link <a href="https://drive.google.com/file/d/1c04kDYCC_Ggf7gSFy-_SuzEDglEmaFnk/view?usp=sharing">to the PDF!</a></p>
         // </object>
